9e492958473b52b05874e232a65cd9875a0bd7d0
Dominic Chu <dominic.m.chu@gmail.com>
2019-11-21 10:21:40 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,28 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do 
+      erb :root
+    end 
+
+    get '/new' do
+      erb:'pirates/new'
+    end 
+
+    post '/pirates' do
+      @pirate = params[:pirate]
+      @p_name = @pirate[:name]
+      @p_weight = @pirate[:weight]
+      @p_height = @pirate[:height]
+      @s1_name = @pirate[:ships][0][:name]
+      @s1_type = @pirate[:ships][0][:type]
+      @s1_booty = @pirate[:ships][0][:booty]
+      @s2_name = @pirate[:ships][1][:name]
+      @s2_type = @pirate[:ships][1][:type]
+      @s2_booty = @pirate[:ships][1][:booty]
+      erb:'/pirates/show'
+    end 
+    
+
   end
 end
